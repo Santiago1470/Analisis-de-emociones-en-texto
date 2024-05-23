9e225a7b86771b9b3c9e6ae39a20d50572d49dd4
Santiago Jair Torres Rivera <67245823+Santiago1470@users.noreply.github.com>
2024-05-23 15:43:25 -0500

    Update analizar.py
    Se eliminaron comentarios

--- analizar.py
+++ analizar.py
@@ -6,21 +6,6 @@ from nltk.tokenize import sent_tokenize
 
 clasificador = pipeline('sentiment-analysis', model='nlptown/bert-base-multilingual-uncased-sentiment')
 
-# tokenizer = BertTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-
-# def analizar_emociones_texto(texto, segment_size=512):
-#     # Dividir el texto en segmentos
-#     segmentos = [texto[i:i+segment_size] for i in range(0, len(texto), segment_size)]
-    
-#     # Analizar emociones para cada segmento
-#     resultados_segmentos = []
-#     for segmento in segmentos:
-#         resultado_segmento = clasificador(segmento)
-#         resultados_segmentos.append(resultado_segmento)
-    
-#     return resultados_segmentos
-
-
 def analizar_sentimiento_hf(texto):
     sentences = sent_tokenize(texto)
     # print(sentences)
@@ -34,11 +19,4 @@ def analizar_sentimiento_hf(texto):
     resultado[len(resultado) - 1]['score'] = prom
     # print(resultado[len(resultado) - 1]['score'])
     return resultado, resultado[len(resultado) - 1]['label']
-
-# def analizar_sentimiento_hf(texto):
-#     resultado = clasificador(texto)
-#     return resultado, resultado[0]['label']
-
-# texto = "bueno"
-# resultado = analizar_sentimiento_hf(texto)
-# print(resultado)
+    
