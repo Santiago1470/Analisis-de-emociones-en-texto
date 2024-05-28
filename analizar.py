a4cd8824005fa6552269c9d8326170e84714df62
Santiago1470 <st147086@gmail.com>
2024-05-27 22:12:26 -0500

    Cambios en la tokenización
    Se hacer la tokenización, es decir, evaluar por oraciones cuando el texto ingresado tiene más de 500 palabras, antes de eso, se hará sin separar el texto.

--- analizar.py
+++ analizar.py
@@ -1,22 +1,27 @@
 from transformers import pipeline
 # from transformers import BertTokenizer
-from nltk.tokenize import sent_tokenize
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 # python -m nltk.downloader all
+nltk.download("punkt")
 
 clasificador = pipeline('sentiment-analysis', model='nlptown/bert-base-multilingual-uncased-sentiment')
 
 def analizar_sentimiento_hf(texto):
-    sentences = sent_tokenize(texto)
-    # print(sentences)
-    resultado = clasificador(sentences)
-    # print(resultado)
-    prom = 0
-    for i in range(len(resultado)):
-        prom += resultado[i]['score']
-    prom = prom / len(resultado)
-    # print(prom)
-    resultado[len(resultado) - 1]['score'] = prom
-    # print(resultado[len(resultado) - 1]['score'])
+    if len(word_tokenize(texto)) > 500:
+        sentences = sent_tokenize(texto)
+        # print(sentences)
+        resultado = clasificador(sentences)
+        # print(resultado)
+        prom = 0
+        for i in range(len(resultado)):
+            prom += resultado[i]['score']
+        prom = prom / len(resultado)
+        # print(prom)
+        resultado[len(resultado) - 1]['score'] = prom
+        # print(resultado[len(resultado) - 1]['score'])
+    else:
+        resultado = clasificador(texto)
     return resultado, resultado[len(resultado) - 1]['label']
     
